3960ef8f5619915776d275ff2d2026943672c4fa
노우석 <nhowooseok@gmail.com>
2022-11-15 09:34:16 +0900

    firstCommit

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
 # Create your tests here.
+class UserReigsterTestCase(APITestCase):
+    def test_registration(self):
+        url =reverse("user_view")
+        user_data = {
+            "username":"testuser",
+            "fullname":"테스터",
+            "email":"test@naver.com",
+            "password":"password",
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response,response)
+        
